630281831ea8d97c6ec7cd76ba695bcb2231c1d9
Sejal <sejalsinhaofficial@gmail.com>
2026-07-30 17:15:54 +0530

    Merge origin/develop into develop

--- backend/src/main/java/com/dbtraining/reconx/service/TradeAggregator.java
+++ backend/src/main/java/com/dbtraining/reconx/service/TradeAggregator.java
@@ -1,7 +1,7 @@
 package com.dbtraining.reconx.service;
 
 import com.dbtraining.reconx.dto.TradeEvent;
-import com.dbtraining.reconx.model.AuditLogEntry;
+import com.dbtraining.reconx.repository.entity.AuditLogEntry;
 import com.dbtraining.reconx.repository.AuditLogRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
